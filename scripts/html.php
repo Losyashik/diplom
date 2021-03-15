1bb68e56b5204a118a49297a094055083fff27a9
Losyashik <79361289+Losyashik@users.noreply.github.com>
2021-03-16 00:24:44 +0300

    Create html.php

---
+++ scripts/html.php
@@ -0,0 +1,41 @@
+<?php
+    function div($content = null,$attr = null)
+    {
+        return "<div $attr>$content</div>";
+    }
+
+    function form($content = null,$attr = null)
+    {
+        return "<form $attr>$content</form>";
+    }
+    function input($attr = null)
+    {
+        return "<input $attr>";
+    }
+    function table($content = null,$attr = null)
+    {
+        return "<table $attr>$content</table>";
+    }
+    function tr($content = null,$attr = null)
+    {
+        return "<tr $attr>$content</tr>";
+    }
+    function th($content = null,$attr = null)
+    {
+        return "<th $attr>$content</th>";
+    }
+    function td($content = null,$attr = null)
+    {
+        return "<td $attr>$content</td>";
+    }
+    function img($attr = null)
+    {
+        return "<img $attr>";
+    }
+    function a($content = null,$attr = null)
+    {
+        return "<a $attr>$content</a>";
+    }
+    
+    
+    
