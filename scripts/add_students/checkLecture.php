c99f8e56a7e6680e7a14cf987499080fdd8428be
Losyashik <79361289+Losyashik@users.noreply.github.com>
2021-06-07 23:48:37 +0300

    Update checkLecture.php

--- scripts/add_students/checkLecture.php
+++ scripts/add_students/checkLecture.php
@@ -33,13 +33,45 @@ if(mysqli_num_rows($result)>0){
         ";
     }
     else{
-        echo "
-        <script>
-            funcyion edit(){
-                alert('Невозможно создать запись. Проверьте правильность введенных данных') 
+        $data = $link->query("SELECT * FROM gdp WHERE id=".$_POST['gdp'])->fetch_assoc();
+
+        $result = $link->query("SELECT * FROM lecture WHERE gdp_id in(SELECT id FROM gdp WHERE teacher_id = ".$data['teacher_id'].") AND date='".$_POST['date']."' AND pair_number = ".$_POST['pair_number']);
+        if($result->num_rows != 0){
+            echo "
+            <script>
+                function edit(){
+                    alert('Невозможно создать запись. Проверьте правильность введенных данных 1'); 
+                    $('#date_lecture').val('');
+                    $('#pair_number').val('');
+                }
+            </script>
+            ";
+        }
+        else{
+            $result = $link->query("SELECT * FROM lecture WHERE gdp_id in(SELECT id FROM gdp WHERE group_id = ".$data['group_id'].") AND date='".$_POST['date']."' AND pair_number = ".$_POST['pair_number']);
+            if($result->num_rows != 0){
+                echo "
+                <script>
+                    function edit(){
+                        alert('Невозможно создать запись. Проверьте правильность введенных данных2'); 
+                        $('#date_lecture').val('');
+                        $('#pair_number').val('');
+                    }
+                </script>
+                ";
             }
-        </script>
-        ";
+            else{
+                echo "
+                <script>
+                    function edit(){
+                        $('.student_name').removeClass('checked');
+                        lectureId=0;
+                    }
+                </script>
+                ";
+            }
+        }
+        
     }
 }
 else
